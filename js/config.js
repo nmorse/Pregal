d6b2c65a11dcefa328a56a167116dc73a86f48e1
Nate Morse <morse.nate@gmail.com>
2015-11-11 00:04:01 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ System.config({
   },
   //map tells the System loader where to look for things
   map: {
-    app: "./graphlet"
+    app: "./js/graphlet"
   },
   //packages defines our app package
   packages: {
