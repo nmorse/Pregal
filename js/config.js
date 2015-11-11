0ac4624a355ccb1c8129a46ce0fe1c9caaa3f2ad
Nate Morse <morse.nate@gmail.com>
2015-11-10 23:56:18 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+System.config({
+  //use typescript for compilation
+  transpiler: 'typescript',
+  //typescript compiler options
+  typescriptOptions: {
+    emitDecoratorMetadata: true
+  },
+  //map tells the System loader where to look for things
+  map: {
+    app: "./graphlet"
+  },
+  //packages defines our app package
+  packages: {
+    app: {
+      main: './editor.ts',
+      defaultExtension: 'ts'
+    }
+  }
+});
