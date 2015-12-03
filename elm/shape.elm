275ee626be22998101edb1fd43755d3a115a757e
Nate Morse <morse.nate@gmail.com>
2015-12-02 21:26:58 -0500

    Create shape.elm

---
+++ elm/shape.elm
@@ -0,0 +1,37 @@
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+
+--type MyShape =
+type MyList a = Empty | Node a (MyList a)
+mkList : MyList Form -> List Form
+mkList l =
+    case l of
+      Empty ->
+          []
+
+      Node first rest ->
+          List.append [first] (mkList rest)
+
+
+
+--things = (Node 1.3 (Node 2 (Node 3.2 Empty))) 
+things = (mkList
+    ( Node (ngon 4 75
+        |> filled clearGrey
+        |> move (-10,0))
+     ( Node (ngon 5 75
+        |> filled clearGrey
+        |> move (50,10))
+       Empty)))
+
+main : Element
+main =
+  collage 300 300
+    things
+
+
+clearGrey : Color
+clearGrey =
+  rgba 111 111 111 0.6
+
