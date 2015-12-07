a1014c23f382303b82185648e92f607cc43f37a7
Nate Morse <morse.nate@gmail.com>
2015-12-06 20:52:35 -0500

    merge 3

--- elm/merge.elm
+++ elm/merge.elm
@@ -3,13 +3,14 @@ import Window
 import Mouse
 
 
-type Update = MouseMove (Int,Int) | WinDim (Int, Int)
+type Update = MouseMove (Int,Int) | WinDim (Int, Int) | Clicked Bool
 
 updates : Signal Update
 updates =
     Signal.merge
         (Signal.map MouseMove Mouse.position)
-        (Signal.map WinDim Window.dimensions)
+        (Signal.merge (Signal.map WinDim Window.dimensions)
+                     (Signal.map Clicked Mouse.isDown))
 
 main : Signal Element
 main =
@@ -21,4 +22,4 @@ view update =
   case update of
     MouseMove (x,y) -> container x y middle (show (x,y))
     WinDim (w,h) -> container w h middle (show (w,h))
-
+    Clicked (d) -> container 300 400 middle (show d)
