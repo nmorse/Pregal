cf310c30467171ed8ec914f7d45149a3aa1cd8e7
Nate Morse <morse.nate@gmail.com>
2016-10-03 22:12:32 -0400

    rotation of the graph

--- elm017/dot.elm
+++ elm017/dot.elm
@@ -3,7 +3,8 @@ import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
-
+import Math.Matrix4 exposing (..)
+import Math.Vector3 exposing (..)
 
 
 main =
@@ -30,19 +31,17 @@ type alias Edge =
   {from: Int, to: Int, name: String}
 
 type alias Model =
-  {t: Time, st: Time, n: List Node, e: List Edge}
-
+  {t: Time, st: Time, n: List Node, e: List Edge, rot: Float}
 
 init : (Model, Cmd Msg)
 init =
   ({ n=[ {id=1,name="a",view={r=10,pos={x=100,y=100}}}
         ,{id=2,name="b",view={r=10,pos={x=50 ,y=200}}}]
     ,e=[{from=1, to=2, name="connect"}]
-    ,t=0,st=0}, Cmd.none)
+    ,t=0,st=0, rot=0}, Cmd.none)
 
 
 -- UPDATE
-
 
 type Msg
   = Tick Time
@@ -58,32 +57,28 @@ update msg model =
         ({model | t = newTime}, Cmd.none)
 
 
-
 -- SUBSCRIPTIONS
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
-
+  Time.every Time.second Tick
 
 
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
   let
     --angle =
     --  turns (Time.inSeconds model.t)
-    slew =
-      round (Time.inSeconds (model.t - model.st))
+    rot_trans =
+      Math.Matrix4.rotate ((Time.inSeconds (model.t - model.st))*1) (Math.Vector3.vec3 30.0 60.0 20.0) Math.Matrix4.identity
   in
     svg [ viewBox "0 0 500 300", width "500px" ]
       (
         List.append
-          (List.map (viewEdge model.n slew) model.e)
-          (List.map (viewNode slew) model.n)
+          (List.map (viewEdge model.n rot_trans) model.e)
+          (List.map (viewNode rot_trans) model.n)
       )
 
 viewEdge all_nodes t edge =
@@ -98,14 +93,14 @@ viewEdge all_nodes t edge =
     get_x n =
       case n of
         Just {id, name, view} ->
-          view.pos.x + t
+          round (getX (Math.Matrix4.transform t (toVec3 view.pos)))
         _ ->
           20
     get_y: Maybe Node -> Int
     get_y n =
       case n of
         Just {id, name, view} ->
-          view.pos.y
+          round (getY (Math.Matrix4.transform t (toVec3 view.pos)))
         _ ->
           20
     mx1 =
@@ -117,15 +112,18 @@ viewEdge all_nodes t edge =
     my2 =
       toString (get_y to_n)
   in
-  line [ x1 mx1, y1 my1, x2 mx2, y2 my2, stroke "#0000aa" ] []
+    line [ x1 mx1, y1 my1, x2 mx2, y2 my2, stroke "#0000aa" ] []
 
 viewNode t n =
   let
     xpos =
-      toString (n.view.pos.x + t)
+      toString (getX (Math.Matrix4.transform t (toVec3 n.view.pos)))
     ypos =
-      toString n.view.pos.y
+      toString (getY (Math.Matrix4.transform t (toVec3 n.view.pos)))
     radius =
       toString n.view.r
   in
     circle [ cx xpos, cy ypos, r radius, fill "#dd0000"] []
+
+toVec3 pos =
+  Math.Vector3.vec3 (toFloat pos.x) (toFloat pos.y) 0.0
